2a9ae5928ea50b4d7cbb56f5527a2747af3f9144
Zed <ZedMoster1@gmail.com>
2022-11-24 16:24:51 +0800

    CmdElementTransformUtils

--- CsharpDemo/Cmd/Cmd.cs
+++ CsharpDemo/Cmd/Cmd.cs
@@ -1,10 +1,8 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
-using Autodesk.Revit.UI.Selection;
-using CommunityToolkit.Mvvm.DependencyInjection;
+using CsharpDemo.Extension;
 using CsharpDemo.Utils;
-using System.Text;
-using System.Windows;
+using System;
 
 namespace CsharpDemo.Cmd
 {
@@ -13,24 +11,8 @@ namespace CsharpDemo.Cmd
     {
         public override void Action()
         {
-            var uidoc = Uidoc;
-            var doc = Uidoc.Document;
+            
 
-            Reference r = uidoc.Selection.PickObject(ObjectType.Element);
-            Element element = doc.GetElement(r);
-            Element elementType = doc.GetElement(element.GetTypeId());
-            Parameter parameter = element.get_Parameter(BuiltInParameter.ALL_MODEL_MARK);
-            StringBuilder sb = new StringBuilder();
-            sb.AppendLine($"Element Name:{element.Name}-ID:{element.Id}");
-            sb.AppendLine($"Total Parameter Instance: {element.Parameters.Size}");
-            sb.AppendLine($"Total Parameter Type: {elementType.Parameters.Size}");
-            sb.AppendLine($"Parameter Name: {parameter.Definition.Name}");
-            sb.AppendLine($"Parameter Group: {parameter.Definition.ParameterGroup}");
-            sb.AppendLine($"Parameter Type: {parameter.Definition.ParameterType}");
-            sb.AppendLine($"Parameter StorageType: {parameter.StorageType}");
-            sb.AppendLine($"Parameter Id: {parameter.Id}");
-            sb.AppendLine($"Parameter IsShared: {parameter.IsShared}");
-            MessageBox.Show(sb.ToString());
         }
     }
 }
