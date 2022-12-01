d9e3ccadad8391e6a7f118478eb04dfe84cc222b
ZedMoster <ZedMoster1@gmail.com>
2022-12-01 21:28:27 +0800

    CmdBoundingBoxIntersectsFilter

---
+++ CsharpDemo/Cmd/CmdPullDowButton/CmdBoundingBoxIntersectsFilter.cs
@@ -0,0 +1,53 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using CsharpDemo.Attributes;
+using CsharpDemo.Extension;
+using CsharpDemo.Utils;
+
+namespace CsharpDemo.Cmd.CmdPullDowButton
+{
+    [Xml("Box过滤器")]
+    [Transaction(TransactionMode.Manual)]
+    public class CmdBoundingBoxIntersectsFilter : RevitCommand
+    {
+        public override void Action()
+        {
+            // 	Autodesk.Revit.DB.BoundingBoxIntersectsFilter Constructor (Outline)
+            var doc = Uidoc.Document;
+            var sel = Uidoc.Selection;
+            var r = sel.PickObject(Autodesk.Revit.UI.Selection.ObjectType.Element,
+                doc.FilterElement(o => o is Floor));
+            var floor = r.GetElement(doc) as Floor;
+            var walls = doc.TBoundingBoxIntersectsFilter<Wall>(floor, BuiltInCategory.OST_Walls);
+            doc.Transaction(t => walls.ForEach(w => JoinTwoElement(doc, floor, w)), "自动连接");
+        }
+
+        /// <summary>
+        /// 两个对象进行连接
+        /// </summary>
+        /// <param name="eleA">对象A 主要</param>
+        /// <param name="eleB">对象B 次要</param>
+        /// <returns>是否成功连接</returns>
+        public bool JoinTwoElement(Document doc, Element eleA, Element eleB)
+        {
+            if (JoinGeometryUtils.AreElementsJoined(doc, eleA, eleB))
+            {
+                if (!JoinGeometryUtils.IsCuttingElementInJoin(doc, eleA, eleB))
+                {
+                    JoinGeometryUtils.SwitchJoinOrder(doc, eleA, eleB);
+                    return true;
+                }
+            }
+            else
+            {
+                JoinGeometryUtils.JoinGeometry(doc, eleA, eleB);
+                if (!JoinGeometryUtils.IsCuttingElementInJoin(doc, eleA, eleB))
+                {
+                    JoinGeometryUtils.SwitchJoinOrder(doc, eleA, eleB);
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ CsharpDemo/Extension/ReferenceExtension.cs
@@ -0,0 +1,18 @@
+﻿using Autodesk.Revit.DB;
+
+namespace CsharpDemo.Extension
+{
+    public static class ReferenceExtension
+    {
+        /// <summary>
+        /// reference 2 element
+        /// </summary>
+        /// <param name="reference"></param>
+        /// <param name="doc"></param>
+        /// <returns></returns>
+        public static Element GetElement(this Reference reference, Document doc)
+        {
+            return doc.GetElement(reference);
+        }
+    }
+}
