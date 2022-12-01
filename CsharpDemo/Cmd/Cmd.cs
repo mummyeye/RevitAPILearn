5c7cf6d80c427d1d7b933450eb78431a9d10eb1f
ZedMoster <ZedMoster1@gmail.com>
2022-12-01 19:31:52 +0800

    cmd

--- CsharpDemo/Cmd/Cmd.cs
+++ CsharpDemo/Cmd/Cmd.cs
@@ -1,17 +1,18 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
+using CsharpDemo.Attributes;
 using CsharpDemo.Extension;
 using CsharpDemo.Utils;
 using System;
 
 namespace CsharpDemo.Cmd
 {
+    [Xml("名称")]
     [Transaction(TransactionMode.Manual)]
     class Cmd : RevitCommand
     {
         public override void Action()
-        {
-            
+        { 
 
         }
     }
