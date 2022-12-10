d6f3b06474315a2696e6e02b2d0f03be614a7b79
ZedMoster <ZedMoster1@gmail.com>
2022-12-10 20:04:57 +0800

    Cmd walljoinat

--- CsharpDemo/Cmd/Cmd.cs
+++ CsharpDemo/Cmd/Cmd.cs
@@ -1,30 +1,18 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
-using Autodesk.Revit.DB.Structure;
 using CsharpDemo.Attributes;
-using CsharpDemo.Extension;
 using CsharpDemo.Utils;
 
 namespace CsharpDemo.Cmd
 {
-    [Xml("梁端点连接")]
+    [Xml("墙端点连接")]
     [Transaction(TransactionMode.Manual)]
     class Cmd : RevitCommand
     {
         public override void Action()
         {
             var doc = Uidoc.Document;
-            var beams = doc.OfClass<FamilyInstance>(BuiltInCategory.OST_StructuralFraming);
-            doc.Transaction(t =>
-            {
-                foreach (var beam in beams)
-                {
-                    StructuralFramingUtils.DisallowJoinAtEnd(beam, 0);
-                    StructuralFramingUtils.DisallowJoinAtEnd(beam, 1);
-                    //StructuralFramingUtils.AllowJoinAtEnd(beam, 0);
-                    //StructuralFramingUtils.AllowJoinAtEnd(beam, 1);
-                }
-            }, "梁端点连接");
+
         }
     }
 }
---
+++ CsharpDemo/Cmd/CmdPullDowButton/CmdWallJoinAt.cs
@@ -0,0 +1,44 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using CsharpDemo.Attributes;
+using CsharpDemo.Extension;
+using CsharpDemo.Utils;
+
+namespace CsharpDemo.Cmd
+{
+    [Xml("墙端点连接")]
+    [Transaction(TransactionMode.Manual)]
+    class CmdWallJoinAt : RevitCommand
+    {
+        public override void Action()
+        {
+            var doc = Uidoc.Document;
+            var walls = doc.OfClass<Wall>();
+            doc.Transaction(t =>
+            {
+                foreach (var wall in walls)
+                {
+                    JoinAt(wall, 0);
+                    JoinAt(wall, 1);
+                }
+            }, "墙端点连接");
+        }
+
+        /// <summary>
+        /// 墙端部连接状态
+        /// </summary>
+        /// <param name="wall"></param>
+        /// <param name="index"></param>
+        private static void JoinAt(Wall wall, int index)
+        {
+            if (WallUtils.IsWallJoinAllowedAtEnd(wall, index))
+            {
+                WallUtils.DisallowWallJoinAtEnd(wall, index);
+            }
+            else
+            {
+                WallUtils.AllowWallJoinAtEnd(wall, index);
+            }
+        }
+    }
+}
