682b48d1732d624f305196cca85a82507299a8e1
Zed <ZedMoster1@gmail.com>
2022-12-14 17:30:30 +0800

    CmdRevisionCloud

---
+++ CsharpDemo/Cmd/CmdPullDowButton2/CmdExportImage.cs
@@ -0,0 +1,26 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using CsharpDemo.Attributes;
+using CsharpDemo.Utils;
+
+namespace CsharpDemo.Cmd.CmdPullDowButton2
+{
+    [Xml("导出图片")]
+    [Transaction(TransactionMode.Manual)]
+    public class CmdExportImage : RevitCommand
+    {
+        public override void Action()
+        {
+            var doc = Uidoc.Document;
+            var opt = new ImageExportOptions();
+            opt.FilePath = "F:\\CmdExportImage0.jpg";
+            doc.ExportImage(opt);
+            opt.FilePath = "F:\\CmdExportImage1.jpg";
+            opt.Zoom = 100;
+            opt.ZoomType = ZoomFitType.FitToPage;
+            opt.PixelSize = 1920;
+            opt.ImageResolution = ImageResolution.DPI_300;
+            doc.ExportImage(opt);
+        }
+    }
+}
---
+++ CsharpDemo/Cmd/CmdPullDowButton2/CmdRevisionCloud.cs
@@ -0,0 +1,46 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using CsharpDemo.Attributes;
+using CsharpDemo.Extension;
+using CsharpDemo.Utils;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CsharpDemo.Cmd.CmdPullDowButton2
+{
+    [Xml("批注云线")]
+    [Transaction(TransactionMode.Manual)]
+    public class CmdRevisionCloud : RevitCommand
+    {
+        public override void Action()
+        {
+            var doc = Uidoc.Document;
+            var sel = Uidoc.Selection;
+            while (true)
+            {
+                try
+                {
+                    var box = sel.PickBox(Autodesk.Revit.UI.Selection.PickBoxStyle.Directional, "框选云线范围");
+                    doc.Transaction(t =>
+                    {
+                        var curves = new List<Curve>();
+                        var p0 = box.Min;
+                        var p1 = new XYZ(box.Min.X, box.Max.Y, box.Min.Z);
+                        var p2 = box.Max;
+                        var p3 = new XYZ(box.Max.X, box.Min.Y, box.Min.Z);
+                        curves.Add(Line.CreateBound(p0, p1).CreateReversed());
+                        curves.Add(Line.CreateBound(p1, p2).CreateReversed());
+                        curves.Add(Line.CreateBound(p2, p3).CreateReversed());
+                        curves.Add(Line.CreateBound(p3, p0).CreateReversed());
+                        var revision = doc.OfClass<Revision>().FirstOrDefault();
+                        RevisionCloud.Create(doc, doc.ActiveView, revision.Id, curves);
+                    }, "批注云线");
+                }
+                catch
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
