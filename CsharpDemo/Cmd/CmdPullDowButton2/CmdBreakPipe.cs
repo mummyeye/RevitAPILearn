96b95444c3f54b1f845dcf191ba8522974f4894d
Zed <ZedMoster1@gmail.com>
2022-12-16 10:51:01 +0800

    cmd break curve

--- CsharpDemo/Cmd/CmdPullDowButton2/CmdBreakPipe.cs
+++ CsharpDemo/Cmd/CmdPullDowButton2/CmdBreakPipe.cs
@@ -4,7 +4,6 @@ using Autodesk.Revit.DB.Plumbing;
 using CsharpDemo.Attributes;
 using CsharpDemo.Extension;
 using CsharpDemo.Utils;
-using System.Linq;
 
 namespace CsharpDemo.Cmd.CmdPullDowButton2
 {
@@ -15,15 +14,7 @@ namespace CsharpDemo.Cmd.CmdPullDowButton2
         public override void Action()
         {
             //PlumbingUtils.BreakCurve
-            var doc = Uidoc.Document;
-            var sel = Uidoc.Selection;
-            var center = default(XYZ);
-            doc.Transaction(t =>
-            {
-                var ref1 = sel.PickObject(Autodesk.Revit.UI.Selection.ObjectType.Element, doc.FilterElement(e => e is Pipe));
-                var pipe1 = doc.GetElement(ref1) as Pipe;
-                var p1Id = PlumbingUtils.BreakCurve(doc, pipe1.Id, ref1.GlobalPoint);
-            });
+
         }
     }
 }
